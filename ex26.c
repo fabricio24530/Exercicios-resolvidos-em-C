96c070fe81ce30ff9f2705589b4bc8d2982c8b7a
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2019-06-24 16:18:24 -0300

    Update ex26.c

--- ex26.c
+++ ex26.c
@@ -1,4 +1,5 @@
-Faça um programa que leia números inteiros até que seja informado o valor 0. Apresente a média dos valores, o maior e o menor valor e a quantidade de números pares e ímpares.
+Faça um programa que leia números inteiros até que seja informado o valor 0. Apresente a média dos valores, o maior e o menor
+valor e a quantidade de números pares e ímpares.
 
 #include <stdio.h>
 #include <stdlib.h>
