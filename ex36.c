23c8888fa92e2998b14324c8d08a050725c8dfab
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2019-06-27 14:46:32 -0300

    Divisores de um numero num intervalo

---
+++ ex36.c
@@ -0,0 +1,19 @@
+ Elabore um programa que apresente todos os números divisíveis por 3 que sejam menores que 100.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if ( i%3 == 0)
+        {
+            printf("%i ", i);
+        }
+    }
+
+    return 0;
+}
