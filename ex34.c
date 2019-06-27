23b0aaaf79320cb0f41cdbc96192ae8c2fb3d0a6
Fabricio Paiva <40863175+fabricio24530@users.noreply.github.com>
2019-06-27 14:32:07 -0300

    Update ex34.c

--- ex34.c
+++ ex34.c
@@ -1,6 +1,6 @@
-Construa um programa que possibilite calcular a área total de uma residência (sala, cozinha, banheiro, quartos etc.).
+/*Construa um programa que possibilite calcular a área total de uma residência (sala, cozinha, banheiro, quartos etc.).
 O programa deve solicitar a entrada do nome, a largura e o comprimento de um determinado cômodo até que o nome do cômodo 
-seja "FIM". O programa deve apresentar o valor total acumulado da área residencial.
+seja "FIM". O programa deve apresentar o valor total acumulado da área residencial.*/
 
 #include <stdio.h>
 #include <stdlib.h>
